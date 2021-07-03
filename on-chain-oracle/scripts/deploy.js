b87ee33ae31b683182b958eb1eda9d2227bf3498
Sutee Sudprasert <sutee.s@gmail.com>
2021-07-03 07:42:54 +0700

    change Oracle address

--- on-chain-oracle/scripts/deploy.js
+++ on-chain-oracle/scripts/deploy.js
@@ -5,7 +5,9 @@ const Oracle = artifacts.require("Oracle");
 async function main() {
     const accounts = await web3.eth.getAccounts();
     const deployer = accounts[0];
-    const offChainOracle = accounts[8];
+    const oracle1 = accounts[1];
+    const oracle2 = accounts[2];
+    const oracle3 = accounts[3];
     console.log("Deploying contracts with the account:", deployer);
     let balance = new BigNumber(await web3.eth.getBalance(deployer));
     console.log(
@@ -14,8 +16,18 @@ async function main() {
     );    
     const oracle = await Oracle.new();
     console.log("Oracle address: ", oracle.address);
-    await oracle.addOracle(offChainOracle);
-    console.log("Add off-chain oracle address:", offChainOracle);
+    
+    await oracle.addOracle(deployer);
+    console.log("Add off-chain oracle address:", deployer);
+
+    await oracle.addOracle(oracle1);
+    console.log("Add off-chain oracle address:", oracle1);
+
+    await oracle.addOracle(oracle2);
+    console.log("Add off-chain oracle address:", oracle2);
+
+    await oracle.addOracle(oracle3);
+    console.log("Add off-chain oracle address:", oracle3);
 }
 
 main()
